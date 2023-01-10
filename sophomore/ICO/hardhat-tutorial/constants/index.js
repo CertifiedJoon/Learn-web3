12f0d05d252b62aa48c7956c7b5da361f9a7e509
Joon_Thinkpad <mjypark1212@gmail.com>
2023-01-10 09:43:26 +0400

    ICO project

---
+++ sophomore/ICO/hardhat-tutorial/constants/index.js
@@ -0,0 +1,5 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS =
+  "0x2A43e3E8ba1CdB3d1B6136Fcd3124eeaF5E76696";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
